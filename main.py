68e456fcc9ce7f4cad093bc8a4e9bac3ece7103e
Dvir kopolovich <64788488+dvirko@users.noreply.github.com>
2022-05-16 14:40:42 +0300

    'setup'

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
 import pymongo
+from bson import json_util
+
 from password import *
 from flask import *
 
 app = Flask(__name__)
+client = pymongo.MongoClient(link)
+db = client.get_database(database)
+col = db.get_collection('user')
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def get_data():
-    client = pymongo.MongoClient(link)
-    db = client.get_database(database)
-    col = db.get_collection('user')
     for doc in col.find():
         for value in doc.values():
             print(value)
-    client.close()
-    return 'all_users'
+    return jsonify(json_util.dumps(list(col.find())))
 
 
 @app.route('/add/<name>', methods=['POST', 'GET'])
 def post_data(name):
-    client = pymongo.MongoClient(link)
-    db = client.get_database(database)
-    col = db.get_collection('user')
     add = {"name": name}
     col.insert_one(add)
-    client.close()
-
-    return 'add '+name
+    return 'add ' + name
 
 
 if __name__ == '__main__':
